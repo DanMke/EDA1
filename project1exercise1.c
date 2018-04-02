8d8c15e42b125daee8ca098c38294b3229bec2f2
Daniel Maike <danmke@hotmail.com>
2018-04-02 18:57:30 -0300

    first commit

---
+++ project1exercise1.c
@@ -0,0 +1,35 @@
+/*
+a) Escreva um programa principal que solicita ao usuário 10 valores
+reais que são guardados em um vetor ENTRADAS e outros 10
+valores reais que são guardados em um vetor PESOS. O programa
+também deve solicitar ao usuário o valor do limiar T. Em seguida a
+função “fneuronio” descrita abaixo deve ser chamada.
+b) Escreva uma função “fneuronio”, que recebe por referência
+(utilizando ponteiros) os vetores ENTRADAS e PESOS, preenchidos
+pelo usuário, além dos valores do limiar T e do número máximo de
+elementos do vetor ENTRADAS, e retorna (utilizando ponteiros) o
+valor 1 caso o neurônio esteja excitado ou 0 caso o neurônio esteja
+inibido.
+c) No programa principal, verificar se o valor de retorno da função
+“fneuronio” foi 1 ou 0 e escrever na tela do computador a
+mensagem “Neurônio ativado!” ou “Neurôno inibido!”,
+respectivamente.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+int fneuronio(*entradas, *pesos, limiarT, valorMax, *excitada);
+
+int main (int argc, char *argv[]) {
+  int i;
+  float
+
+
+  return 0;
+}
+
+int fneuronio(*entradas, *pesos, limiarT, valorMax, *excitada) {
+
+}
